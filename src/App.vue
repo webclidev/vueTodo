a183084de62983380f3c850ba785e5e26b834600
webclidev <devkant.dhruv@gmail.com>
2018-11-22 08:55:40 +0530

    todo app with material design

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,71 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-app App >
+     
+      <!--############################# startvuetify ###########################-->
 
+ <v-app id="inspire" dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      fixed
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Todo App</v-toolbar-title>
+    </v-toolbar>
     <v-content>
-      <HelloWorld/>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <todo></todo>
+        </v-layout>
+      </v-container>
     </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2018</span>
+    </v-footer>
   </v-app>
+
+
+      <!--############################# ends startvuetify ###########################-->
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+// import HelloWorld from './components/HelloWorld'
+import todo from "./components/todo";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    todo
   },
-  data () {
+  data() {
     return {
+      drawer: closed
       //
-    }
+    };
+  },
+  props: {
+    source: String
   }
-}
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import store from './store'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
-
+Vue.use(Vuex)
+Vue.use(Vuetify)
 new Vue({
   render: h => h(App),
   store
